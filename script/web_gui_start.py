1c0b0308659da6f67993e8062af611de8494c8bb
mjlee111 <menggu1234@naver.com>
2024-07-29 16:25:31 +0900

    feat: js load update

--- script/web_gui_start.py
+++ script/web_gui_start.py
@@ -2,6 +2,8 @@
 import http.server
 import socketserver
 import os
+import signal
+import sys
 
 PORT = 8000
 DIRECTORY = os.path.join(os.path.dirname(__file__), '../html')
@@ -10,6 +12,24 @@ class CustomHandler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory=DIRECTORY, **kwargs)
 
-with socketserver.TCPServer(("", PORT), CustomHandler) as httpd:
-    print(f"Serving at port {PORT}")
-    httpd.serve_forever()
+def signal_handler(sig, frame):
+    print("\nServer is shutting down...")
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
+
+httpd = None
+
+try:
+    with socketserver.TCPServer(("", PORT), CustomHandler) as httpd:
+        print(f"Serving at port {PORT}")
+        httpd.serve_forever()
+except KeyboardInterrupt:
+    print("\nServer interrupted by user")
+except Exception as e:
+    print(f"An error occurred: {e}")
+finally:
+    if httpd:
+        httpd.server_close()
+        print("Server closed")
